f0ba1bda1b31fbf6be47ee23592f390299fdb63e
Cesare-Sari <cesaresari2003@gmail.com>
2019-02-20 10:22:52 +0100

    creato form di partenza

--- index.php
+++ index.php
@@ -11,9 +11,33 @@
     <title>Hello, world!</title>
   </head>
   <body>
-    <h1>Hello, world!</h1>
 
 
+ <div class="container">   
+  <div class="rows">
+   <div class="col-12"> 
+    <form>
+      <div class="form-group">
+        <label for="Nome">Nome</label>
+        <input type="text" class="form-control" id="Nome" placeholder="inserisci Nome">
+      </div>
+
+      <div class="form-group">
+        <label for="Cognome">Cognome</label>
+        <input type="text" class="form-control" id="Cognome" placeholder="inserisci Cognome">
+      </div>
+     
+      <div class="form-group">
+        <label for="indirizzo">Indirizzo</label>
+        <textarea class="form-control" id="indirizzo" rows="3" placeholder="inserisci Email" ></textarea>
+      </div>
+
+
+      <button type="submit" class="btn btn-primary">Invia</button>
+    </form>
+   </div>
+  </div>
+ </div>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="js/jquery-3.3.1.min.js"></script>
