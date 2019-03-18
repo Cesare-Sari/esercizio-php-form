64ce583348b1e9a9f5bc8b39d0b9570275b46832
Cesare-Sari <cesaresari2003@gmail.com>
2019-03-18 11:13:59 +0100

    modifiche

--- index.php
+++ index.php
@@ -1,10 +1,8 @@
 <?php  
 
-
-  $nome = (isset($_POST['Nome'])) ?
-  $_POST['Nome'] : '';
-
-  print $nome; 
+  $nome = (isset($_POST['nome'])) ? $_POST['nome'] : '';
+  $cognome = (isset($_POST['cognome'])) ? $_POST['cognome'] : '';
+  $indirizzo = (isset($_POST['indirizzo'])) ? $_POST['indirizzo'] : '';
 
 ?>  
 
@@ -25,35 +23,55 @@
 
     <title>Hello, world!</title>
   </head>
-  <body>
+ <body>
 
 
- <div class="container">   
-  <div class="rows">
-   <div class="col-12"> 
-    <form action="index.php" method="post">
-      <div class="form-group">
+  <div class="container">   
+   
+
+   <div class="alert alert-danger" role="alert">
+  
+    <?php 
+
+    print $nome . '<br>';
+    print $cognome . '<br>';
+    print $indirizzo . '<br>';
+
+    ?>
+
+   </div>
+ 
+
+
+
+   <div class="rows">
+    <div class="col-12"> 
+     <form action="index.php" method="post">
+       <div class="form-group">
         <label for="Nome">Nome</label>
-        <input type="text" name="Nome" class="form-control" id="Nome" placeholder="inserisci Nome">
-      </div>
+        <input type="text" name="nome" class="form-control" id="Nome" placeholder="inserisci Nome">
+       </div>
 
-      <div class="form-group">
+       <div class="form-group">
         <label for="Cognome">Cognome</label>
-        <input type="text" class="form-control" id="Cognome" placeholder="inserisci Cognome">
-      </div>
+        <input type="text" name="cognome" class="form-control" id="Cognome" placeholder="inserisci Cognome">
+       </div>
      
-      <div class="form-group">
+       <div class="form-group">
         <label for="indirizzo">Indirizzo</label>
-        <textarea class="form-control" id="indirizzo" rows="3" placeholder="inserisci indirizzo" ></textarea>
-        <small id="indirizzoHelp" class="form-text text-muted" >il tuo indirizzo deve essere reale.</small>
-      </div>
+        <textarea class="form-control" name="indirizzo" id="indirizzo" rows="3" placeholder="inserisci indirizzo" ></textarea>
+        <small id="indirizzoHelp"  class="form-text text-muted" >il tuo indirizzo deve essere reale.</small>
+       </div>
 
 
-      <button type="submit" class="btn btn-primary">Invia</button>
-    </form>
+       <button type="submit" class="btn btn-primary">Invia</button>
+     </form>
+    </div>
    </div>
   </div>
- </div>
+
+ 
+
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="js/jquery-3.3.1.min.js"></script>
